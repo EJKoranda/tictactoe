059a308dc01a6a03e1a06c34104b7d1fcddbee18
Ej Koranda <ejk2706@gmail.com>
2025-05-03 20:10:56 -0700

    write up while loops to keep players within array

--- app.js
+++ app.js
@@ -5,14 +5,21 @@ let board = [
 ]
 while (true) {
     let input = prompt("What would you like to do");
+
     if (input === "new") {
-        let row = parseInt(prompt("Input the row to access"));
-        let column = parseInt(prompt("Now please enter the column to access"));
-        newValue = prompt("Now enter what you would like to input in that column");
-        board[row][column] = newValue;
+        let row = prompt("Input the row to access");
+        while (parseInt(row) > board.length - 1 || row.trim() === "" || parseInt(row) < 0) {
+            row = prompt("Please input your row choice with respects to the borders")
+        }
+        let column = prompt("Now please enter the column to access");
+        while (parseInt(column) > board.length - 1 || column.trim() === "" || parseInt(column) < 0) {
+            column = prompt("Please input your column choice with respects to the borders")
+        }
+        let newValue = prompt("Now enter what you would like to input in that column");
+        board[parseInt(row)][parseInt(column)] = newValue;
         console.log(`Your board was succesfully edited at row: ${row} and column: ${column}`);
     }
-    if (input === "display") {
+    if (input === "d" || input === "display") {
         console.log("Your board currently looks like this");
         console.log("--------------------------------------")
         for (let i = 0; i < board.length; i++) {
@@ -24,4 +31,5 @@ while (true) {
         console.log("quit");
         break;
     }
+
 }
